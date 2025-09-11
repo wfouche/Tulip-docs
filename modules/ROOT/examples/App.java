78c370b9564d40144e1918a52eae358027957653
Werner Fouché <werner.fouche@gmail.com>
2025-09-11 20:04:03 +0200

    Moved example program to App.java

---
+++ modules/ROOT/examples/App.java
@@ -0,0 +1,11 @@
+///usr/bin/env jbang "$0" "$@" ; exit $\?
+//DEPS io.github.wfouche.tulip:tulip-runtime:2.1.9
+//JAVA 21
+
+import io.github.wfouche.tulip.api.TulipApi;
+
+public class App {
+  public static void main(String[] args) {
+    TulipApi.runTulip("benchmark_config.json");
+  }
+}
