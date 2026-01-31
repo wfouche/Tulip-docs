39a1367ea0ed403238a815b4f2db147c4b46a381
Werner Fouché <werner.fouche@gmail.com>
2026-01-31 09:57:34 +0200

    Release 2.2.0

--- modules/ROOT/examples/App.java
+++ modules/ROOT/examples/App.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $\?
-//DEPS io.github.wfouche.tulip:tulip-runtime:2.1.12
+//DEPS io.github.wfouche.tulip:tulip-runtime:2.2.0
 //JAVA 21
 
 import io.github.wfouche.tulip.api.TulipApi;
